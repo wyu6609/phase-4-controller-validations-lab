fcc1a8274c0d0e4cb953564bb6d5c6a7c2064f4b
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-04-04 12:39:37 -0400

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,7 @@
 class AuthorsController < ApplicationController
-  
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid,
+  #             with: :render_unprocessable_entity_response
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +9,39 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: {
+             errors: invalid.record.errors,
+           },
+           status: :unprocessable_entity
   end
 
+  # def update
+  #   author = find_author
+  #   author.update(author_params)
+  #   render json: author, status: :ok
+  # end
+
   private
-  
+
+  # def find_author
+  #   Author.find(params[:id])
+  # end
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  # def render_not_found_response
+  #   render json: { error: 'Author not found' }, status: :not_found
+  # end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: {
+  #            errors: invalid.record.errors,
+  #          },
+  #          status: :unprocessable_entity
+  # end
 end
